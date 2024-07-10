1b57c9060dc5147bee1435f10d67ba8e25e0fd37
Eugene <evgenii.evgenii.02@mail.ru>
2024-07-10 22:41:51 +0300

    корректировка ошибок выявленных наставником

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ def get_mask_card_number(card_number: str) -> str | None:
     :return:
     """
     if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:5]} {card_number[5:7]} {"*" * 2} {"*" * 4} {card_number[12:]}"
+        return f"{card_number[:4]} {card_number[4:6]} {"*" * 2} {"*" * 4} {card_number[12:]}"
     else:
         return None
 
