b69d7465408b2a1a85014259ed5ed7a9c9c1e9a2
Eugene <evgenii.evgenii.02@mail.ru>
2024-07-16 21:03:59 +0300

    создание тестов и запуск

--- src/processing.py
+++ src/processing.py
@@ -17,14 +17,7 @@ def filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED")
     return [data_dict for data_dict in list_of_dict if data_dict.get("state") == state]
 
 
-def sort_by_date(list_of_dict: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
-    """
-    Функция принимает на вход список словарей и возвращает новый список, в котором исходные
-    словари отсортированы по убыванию даты
-    """
-    sorted_list = sorted(
-        list_of_dict,
-        key=lambda filtered_dicts: filtered_dicts["date"],
-        reverse=reverse,
-    )
+def sort_by_date(date_list: list, reverse_list: bool = True) -> list | bool:
+    """Return filtered list by date"""
+    sorted_list = sorted(date_list, key=lambda date_dict: date_dict.get("date"), reverse=reverse_list)
     return sorted_list
